5e701cc0734b6109144940435cc271d8002d67df
leogomeslima <leonardo.glima2017@gmail.com>
2025-12-09 06:39:16 -0300

    feat(domain): add Responsavel entity

---
+++ ProjetoIMS.Domain/Entities/Responsavel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoIMS.Domain.Entities
+{
+    public class Responsavel : BaseEntity
+    {
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string? Telefone { get; private set; }
+
+        public virtual ICollection<Asset> Ativos { get; private set; }
+
+        private Responsavel()
+        {
+            Ativos = new List<Asset>();
+        }
+
+        public Responsavel(string nome, string email, string? telefone = null) : this()
+        {
+            Nome = nome;
+            Email = email;
+            Telefone = telefone;
+        }
+
+        public void Update(string nome, string email, string? telefone)
+        {
+            Nome = nome;
+            Email = email;
+            Telefone = telefone;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void Delete()
+        {
+            IsDeleted = true;
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
