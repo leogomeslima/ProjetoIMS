742c7520282d0fd77e91353db631f74e4c070d08
leogomeslima <leonardo.glima2017@gmail.com>
2025-12-09 06:40:47 -0300

    feat(domain): add IUnitOfWork to orchestrate repositories

---
+++ ProjetoIMS.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjetoIMS.Domain.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IAssetRepository Assets { get; }
+        ILocalizacaoRepository Localizacoes { get; }
+        IResponsavelRepository Responsaveis { get; }
+        IMovimentacaoRepository Movimentacoes { get; }
+        Task<int> CommitAsync();
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
+    }
+}
