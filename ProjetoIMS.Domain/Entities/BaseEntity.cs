07968c2cb9c53c79f4ba56e4c3785feee2d8c709
leogomeslima <leonardo.glima2017@gmail.com>
2025-12-09 06:38:28 -0300

    feat(domain): add BaseEntity with common audit fields

---
+++ ProjetoIMS.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoIMS.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; protected set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+
+        protected BaseEntity()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.UtcNow;
+            IsDeleted = false;
+        }
+    }
+}
