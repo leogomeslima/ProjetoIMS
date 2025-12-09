30be0d1adb67781e8ebef32eda740b91292e4265
leogomeslima <leonardo.glima2017@gmail.com>
2025-12-09 06:39:01 -0300

    feat(domain): add Localizacao entity

---
+++ ProjetoIMS.Domain/Entities/Localizacao.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoIMS.Domain.Entities
+{
+    public class Localizacao : BaseEntity
+    {
+        public string Nome { get; private set; }
+        public string? Descricao { get; private set; }
+
+        // Navigation Properties
+        public virtual ICollection<Asset> Ativos { get; private set; }
+
+        private Localizacao()
+        {
+            Ativos = new List<Asset>();
+        }
+
+        public Localizacao(string nome, string? descricao = null) : this()
+        {
+            Nome = nome;
+            Descricao = descricao;
+        }
+
+        public void Update(string nome, string? descricao)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void Delete()
+        {
+            IsDeleted = true;
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+
+}
