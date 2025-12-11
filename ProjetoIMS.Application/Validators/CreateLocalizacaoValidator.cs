6fcc33f94a01ee19f6dce6d294d8dfae44653054
leogomeslima <leonardo.glima2017@gmail.com>
2025-12-11 06:14:50 -0300

    feat(application): add validators for Localizacao and Responsavel DTOs

---
+++ ProjetoIMS.Application/Validators/CreateLocalizacaoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using ProjetoIMS.Application.DTOs.Localizacao;
+
+namespace ProjetoIMS.Application.Validators
+{
+    public class CreateLocalizacaoDtoValidator : AbstractValidator<CreateLocalizacaoDto>
+    {
+        public CreateLocalizacaoDtoValidator()
+        {
+            RuleFor(x => x.Nome)
+                .NotEmpty().WithMessage("Nome é obrigatório")
+                .MaximumLength(200).WithMessage("Nome deve ter no máximo 200 caracteres");
+
+            RuleFor(x => x.Descricao)
+                .MaximumLength(500).When(x => !string.IsNullOrEmpty(x.Descricao))
+                .WithMessage("Descrição deve ter no máximo 500 caracteres");
+        }
+    }
+}
---
+++ ProjetoIMS.Application/Validators/CreateResponsavelValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using ProjetoIMS.Application.DTOs.Responsavel;
+
+namespace ProjetoIMS.Application.Validators
+{
+    public class CreateResponsavelDtoValidator : AbstractValidator<CreateResponsavelDto>
+    {
+        public CreateResponsavelDtoValidator()
+        {
+            RuleFor(x => x.Nome)
+                .NotEmpty().WithMessage("Nome é obrigatório")
+                .MaximumLength(200).WithMessage("Nome deve ter no máximo 200 caracteres");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email é obrigatório")
+                .EmailAddress().WithMessage("Email inválido")
+                .MaximumLength(200).WithMessage("Email deve ter no máximo 200 caracteres");
+
+            RuleFor(x => x.Telefone)
+                .MaximumLength(20).When(x => !string.IsNullOrEmpty(x.Telefone))
+                .WithMessage("Telefone deve ter no máximo 20 caracteres")
+                .Matches(@"^\+?[\d\s\-\(\)]+$").When(x => !string.IsNullOrEmpty(x.Telefone))
+                .WithMessage("Telefone inválido");
+        }
+    }
+}
