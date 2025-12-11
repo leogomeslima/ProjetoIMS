642a8696e50f92c59b41a3d0ebc3ce1b2cde03db
leogomeslima <leonardo.glima2017@gmail.com>
2025-12-10 22:39:08 -0300

    feat(application): add Localizacao DTOs

---
+++ ProjetoIMS.Application/DTOs/Localizacao/CreateLocalizacaoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjetoIMS.Application.DTOs.Localizacao
+{
+    public class CreateLocalizacaoDto
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string? Descricao { get; set; }
+    }
+}
---
+++ ProjetoIMS.Application/DTOs/Localizacao/LocalizacaoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjetoIMS.Application.DTOs.Localizacao
+{
+    public class LocalizacaoDto
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string? Descricao { get; set; }
+        public int QuantidadeAtivos { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
