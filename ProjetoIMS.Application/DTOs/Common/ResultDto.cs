2e881d4748f4e2d94ea0980dec748f409a8f0b95
leogomeslima <leonardo.glima2017@gmail.com>
2025-12-10 22:29:42 -0300

    feat(application): add ResultDto and PagedResultDto

---
+++ ProjetoIMS.Application/DTOs/Common/ResultDto.cs
@@ -0,0 +1,30 @@
+﻿namespace ProjetoIMS.Application.DTOs.Common
+{
+    public class ResultDto<T>
+    {
+        public bool Success { get; set; }
+        public T? Data { get; set; }
+        public string? Message { get; set; }
+        public List<string> Errors { get; set; } = new();
+
+        public static ResultDto<T> SuccessResult(T data, string? message = null)
+        {
+            return new ResultDto<T>
+            {
+                Success = true,
+                Data = data,
+                Message = message
+            };
+        }
+
+        public static ResultDto<T> FailureResult(string message, List<string>? errors = null)
+        {
+            return new ResultDto<T>
+            {
+                Success = false,
+                Message = message,
+                Errors = errors ?? new List<string>()
+            };
+        }
+    }
+}
